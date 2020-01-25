8ea0abd30e2e5392b2bb47fde8523dd269822c89
JEinsel <Joshua.einsel@live.com>
2020-01-25 08:10:54 -0700

    Mostly functioning if dirty auth0

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/AuthTestOne/Main"
+import Secret from "./components/AuthTestOne/Secret"
+import NotFound from "./components/AuthTestOne/NotFound"
+import Callback from "./components/AuthTestOne/Callback"
+import Auth from "./Auth"
 
 class App extends Component {
   render() {
+    let mainComponent = ""
+    switch (this.props.location) {
+      case "":
+
+        mainComponent = <Main {...this.props} />
+
+        break;
+      case "callback":
+        mainComponent = <Callback />
+        break;
+      case "secret":
+        switch (this.props.auth.isAuthenticated) {
+          case true:
+            mainComponent = <Secret {...this.props} />
+            break;
+          case false:
+            mainComponent = <NotFound {...this.props} />
+
+        }
+
+        break;
+      default:
+        mainComponent = <NotFound {...this.props} />
+    }
+
     return (
       <div>
-        <h1>This is the Gathered Here Registry App.</h1>
+        <h1>This is the Gathered Here Registry App, {this.props.name}.</h1>
+
+        {mainComponent}
       </div>
     );
   }
